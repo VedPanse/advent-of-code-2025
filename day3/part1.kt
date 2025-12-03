e9dd66526764aa2ea09b8aeee1ac23ab7cd3e817
VedPanse <panse.ved@yandex.com>
2025-12-02 21:33:44 -0800

    day 3 part 1

---
+++ day3/part1.kt
@@ -0,0 +1,26 @@
+import java.io.File
+import kotlin.math.*
+
+fun main() {
+    var total: Int = 0
+    File("day3/input.txt").forEachLine { line ->
+        var number: Int = line[0].digitToInt() * 10 + line[1].digitToInt()
+        var l: Int = 0
+        var r: Int = 1
+
+        val n: Int = line.length
+
+        while (r < n) {
+            val thisNumber: Int = line[l].digitToInt() * 10 + line[r].digitToInt()
+
+            number = max(thisNumber, number)
+            if (line[l].digitToInt() < line[r].digitToInt()) {
+                l = r
+            }
+            r++
+        }
+        total += number
+    }
+
+    println("total: $total")
+}
